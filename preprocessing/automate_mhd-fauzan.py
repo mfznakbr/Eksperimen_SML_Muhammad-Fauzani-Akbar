ab8e682d097edb74cd4515fe45e8aa656d486854
mfznakbr <fauzanmhd504@gmail.com>
2025-07-15 19:50:34 +0700

    ubah preprocessing

--- preprocessing/automate_mhd-fauzan.py
+++ preprocessing/automate_mhd-fauzan.py
@@ -1,23 +1,36 @@
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
 import pandas as pd
 import argparse
 import os
 
 def preprocess_data(data, path):
+
     # Ambil fitur numerik dan kategorikal
     numeric = data.select_dtypes(include=['float64', 'int64']).columns.tolist()
-    categorical = data.select_dtypes(include=['object']).columns.tolist()
+    categorical = ['Stage_fear', 'Drained_after_socializing']
+    target = 'Personality'
 
+    # encode kolom target
+    le = LabelEncoder()
+    data[target] = le.fit_transform(data[target])
     # bersihkan data dari missing value dan duplicate value
+
     data = data.dropna()
     data = data.drop_duplicates()
+
+    # hindari target masuk ke pipeline
+    if target in numeric:
+        numeric.remove(target)
+    if target in categorical:
+        categorical.remove(target)
 
     # pipeline masing - masing tipe
     onehot_transform = Pipeline([
         ('encoder', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
     ])
+
 
     scaler_transform = Pipeline([
         ('scaler', StandardScaler())
@@ -25,7 +38,7 @@ def preprocess_data(data, path):
 
     prerocessing = ColumnTransformer([
         ('num', scaler_transform, numeric),
-        ('onehot', onehot_transform, categorical)
+        ('onehot', onehot_transform, categorical),
     ])
 
     transform_data = prerocessing.fit_transform(data)
@@ -34,15 +47,15 @@ def preprocess_data(data, path):
 
     all_columns = numeric + list(encod_cols)
 
-
+    # gabung hasil dengan target
     preprocess_df = pd.DataFrame(transform_data, columns=all_columns)
+    preprocess_df[target] = data[target].values
 
     preprocess_df.to_csv(path, index=False)
 
     print("SELESAI EUYYY")
 
     return preprocess_df
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
