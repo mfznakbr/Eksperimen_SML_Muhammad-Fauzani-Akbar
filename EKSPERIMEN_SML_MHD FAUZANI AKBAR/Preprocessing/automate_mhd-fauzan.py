88c677d8d31a7c90c5f8272c4db82fd0a64347e7
mfznakbr <fauzanmhd504@gmail.com>
2025-07-15 12:23:53 +0700

    initial commit

---
+++ EKSPERIMEN_SML_MHD FAUZANI AKBAR/Preprocessing/automate_mhd-fauzan.py
@@ -0,0 +1,48 @@
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import pandas as pd
+
+def preprocess_data(data, path):
+    # Ambil fitur numerik dan kategorikal
+    numeric = data.select_dtypes(include=['float64', 'int64']).columns.tolist()
+    categorical = data.select_dtypes(include=['object']).columns.tolist()
+
+    # bersihkan data dari missing value dan duplicate value
+    data = data.dropna()
+    data = data.drop_duplicates()
+
+    # pipeline masing - masing tipe
+    onehot_transform = Pipeline([
+        ('encoder', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
+    ])
+
+    scaler_transform = Pipeline([
+        ('scaler', StandardScaler())
+    ])
+
+    prerocessing = ColumnTransformer([
+        ('num', scaler_transform, numeric),
+        ('onehot', onehot_transform, categorical)
+    ])
+
+    transform_data = prerocessing.fit_transform(data)
+
+    encod_cols = prerocessing.named_transformers_['onehot']['encoder'].get_feature_names_out(categorical)
+
+    all_columns = numeric + list(encod_cols)
+
+
+    preprocess_df = pd.DataFrame(transform_data, columns=all_columns)
+
+    preprocess_df.to_csv(path, index=False)
+
+    print("SELESAI EUYYY")
+
+    return preprocess_df
+
+
+df = pd.read_csv(r"D:\SMSML_MUHAMMAD FAUZANI AKBAR\EKSPERIMEN_SML_MHD FAUZANI AKBAR\Preprocessing\personality_datasert.csv")
+path = r"D:\SMSML_MUHAMMAD FAUZANI AKBAR\EKSPERIMEN_SML_MHD FAUZANI AKBAR\Preprocessing\personality_preprocessing.csv"
+print(df.head())
+jadi = preprocess_data(df, path)
